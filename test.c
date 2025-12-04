e8b9ff3dfa5fd269305bde2b67d0ce57428e2ddf
cedric-star <cdrc.wnsch@gmail.com>
2025-12-04 16:58:43 +0100

    test sachen c ncurses und nix shell

--- test.c
+++ test.c
@@ -1,21 +1,38 @@
 #include <curses.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <time.h>
+#include <time.h>
+#include <math.h>
+#include <stdlib.h>
 
 
+#define ARR_LENGTH 20
+#define MAX_NUM 10
 
 int main (void) {
   initscr();
   cbreak();
   noecho();
 
-  mvaddch(0, 0, '+');
-  mvaddch(LINES - 1, 0, '-');
+  int arr[ARR_LENGTH];
+
+
+  srand(time(NULL));
+  for (int i = 0; i < ARR_LENGTH; i++) {
+    arr[i] = floor(rand() % MAX_NUM);
+    char str[20];
+    sprintf(str, "%d", arr[i]);
+    mvaddstr(1, (i*2), str);
+  }
+
   mvaddstr(10, 30, "press any key to quit");
+  
+  
+  
+  
   refresh();
-
   getch();
-
   endwin();
   return 0;
 }
